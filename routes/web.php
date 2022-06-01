6729ca5acb35edc1af29f541b29be5aa5d6bb9fb
Oleksii Temchenko <OTemchenko@fxcess.com>
2022-06-02 02:28:28 +0300

    dev auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+require __DIR__.'/auth.php';
