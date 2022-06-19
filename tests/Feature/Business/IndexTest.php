48f8b339183f95e2f298a52184e108c1443279aa
Oleksii Temchenko <OTemchenko@fxcess.com>
2022-06-19 07:07:04 +0300

    dev

--- tests/Feature/Business/IndexTest.php
+++ tests/Feature/Business/IndexTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Business;
+use App\Models\BusinessService;
+use App\Models\Service;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -27,5 +30,19 @@ class IndexTest extends TestCase
         $response = $this->actingAs($user)->get($url);
         $response->assertDontSee('/login');
         $response->assertSee($user->name);
+    }
+
+    public function testSearch()
+    {
+        $user = User::factory()->create();
+        $businesses = Business::factory(1)
+            ->hasAttached(Service::inRandomOrder()->first(), ['aprox_price' => BusinessService::APROX_PRICE_LOW])
+            ->for($user)
+            ->create();
+        $business = $businesses->first();
+
+        $url = route('businesses.search', ['q' => $business->name], false);
+        $response = $this->get($url);
+        $response->assertSee($business->name);
     }
 }
